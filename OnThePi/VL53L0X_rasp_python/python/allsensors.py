2055c828b2681f1eb1dcc42aa8a3eb925b67beba
sbhatnagar30 <sbhatnagar30@gatech.edu>
2018-04-05 23:45:55 +0000

    asasdf stuff about string things

--- OnThePi/VL53L0X_rasp_python/python/allsensors.py
+++ OnThePi/VL53L0X_rasp_python/python/allsensors.py
@@ -10,10 +10,10 @@ addr0_arr = [0x20,0x21,0x22,0x23]
 addr1_arr = [0x24,0x25,0x26,0x27]
 
 tof_arr0 = initBus(gpio0_arr, addr0_arr)
-tof_arr1 = initBus(gpio1_arr, addr1_arr)
+tof_arr1 = initBus(gpio1_arr, addr0_arr)
 
 def prettyPrint(busnum, arr):
-    print 'bus%d: %3s %3s %3s %3s' % busnum, arr[0], arr[1], arr[2], arr[3]
+    print 'bus%d: %3s %3s %3s %3s' % (busnum, arr[0], arr[1], arr[2], arr[3])
 
 while(1):
     zeroBus = readBus(tof_arr0)
