28cc92cfe17e0eef87c749c1375708a1f5e683e8
pwolfe8 <tlee753@gmail.com>
2018-04-05 20:09:40 -0400

    put stuff back

--- OnThePi/VL53L0X_rasp_python/python/allsensors.py
+++ OnThePi/VL53L0X_rasp_python/python/allsensors.py
@@ -9,7 +9,7 @@ gpio1_arr = [12, 16, 19, 21]
 addr_arr = [0x20,0x21,0x22,0x23]
 
 tof_arr0 = initBus(0, gpio0_arr, addr_arr)
-#tof_arr1 = initBus(1, gpio1_arr, addr_arr)
+tof_arr1 = initBus(1, gpio1_arr, addr_arr)
 
 def prettyPrint(busnum, arr):
     print 'bus%d: %3s %3s %3s %3s' % (busnum, arr[0], arr[1], arr[2], arr[3])
@@ -19,9 +19,9 @@ while(1):
     prettyPrint(0, zeroBus)
     time.sleep(delay)
 
-    #oneBus = readBus(tof_arr1)
-    #prettyPrint(1, oneBus)
-    #time.sleep(delay)
+    oneBus = readBus(tof_arr1)
+    prettyPrint(1, oneBus)
+    time.sleep(delay)
     print ()
 
 
