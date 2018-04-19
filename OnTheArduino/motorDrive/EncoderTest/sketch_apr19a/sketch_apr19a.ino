645edc21ba366cd32c9cf2eb13e670ae5d8a7a75
sbhatnagar30 <sbhatnagar30@gatech.edu>
2018-04-19 07:51:53 +0000

    fixed encoder.ino

--- OnTheArduino/motorDrive/EncoderTest/sketch_apr19a/sketch_apr19a.ino
+++ OnTheArduino/motorDrive/EncoderTest/sketch_apr19a/sketch_apr19a.ino
@@ -14,14 +14,14 @@
 #define FL (11)
 #define FL_CONTROL (46)
 
-#define BR_ENC_A (32)
-#define BR_ENC_B (34)
-#define BL_ENC_A (36)
-#define BL_ENC_B (38)
-#define FR_ENC_A (40)
-#define FR_ENC_B (42)
-#define FL_ENC_A (28)
-#define FL_ENC_B (26)
+#define BR_ENC_A (38)
+#define BR_ENC_B (36)
+#define BL_ENC_A (32)
+#define BL_ENC_B (34)
+#define FR_ENC_A (26)
+#define FR_ENC_B (28)
+#define FL_ENC_A (40)
+#define FL_ENC_B (42)
 
 
 ////////////////////////
@@ -44,8 +44,8 @@ volatile int flCountPrev = 0;
 volatile int flRevs = 0;
 const int8_t encoder_table[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
 
-
-
+const uint32_t TEST_TIME_MILLIS = 200000;
+uint32_t stop_millis = 0;
 
 
 ////////////////////////
@@ -64,7 +64,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(BL_ENC_B), encoder_interrupt_bl, CHANGE);
   attachInterrupt(digitalPinToInterrupt(BR_ENC_A), encoder_interrupt_br, CHANGE);
   attachInterrupt(digitalPinToInterrupt(BR_ENC_B), encoder_interrupt_br, CHANGE);
-  
+  stop_millis = millis() + TEST_TIME_MILLIS;
 }
 
 ////////////////////////
@@ -72,16 +72,17 @@ void setup() {
 ////////////////////////
 void loop() {
 
-  // PRINT STATUS ON EACH COUNTER
-  Serial.print("brCount = ");
-  Serial.print(brCount);
-  Serial.print(" blCount = ");
-  Serial.print(blCount);
-  Serial.print(" frCount = ");
-  Serial.print(frCount);
-  Serial.print(" flCount = ");
-  Serial.print(flCount);
-  Serial.print("\n");
+  if (millis() <= stop_millis) {
+    Serial.print("br = ");
+    Serial.print(brCount);
+    Serial.print(" bl = ");
+    Serial.print(blCount);
+    Serial.print(" fr = ");
+    Serial.print(frCount);
+    Serial.print(" fl = ");
+    Serial.print(flCount);
+    Serial.print("\n");
+  }
 
 }
 
@@ -92,23 +93,23 @@ void loop() {
 void encoder_interrupt_br() {
   static uint8_t br_enc_val = 0;
   br_enc_val = (br_enc_val << 2) | (digitalRead(BR_ENC_A) << 1) | digitalRead(BR_ENC_B);
-  brCount = encoder_table[br_enc_val & 0b1111];
+  brCount += encoder_table[br_enc_val & 0b1111];
 }
 
 void encoder_interrupt_bl() {
   static uint8_t bl_enc_val = 0;
   bl_enc_val = (bl_enc_val << 2) | (digitalRead(BL_ENC_A) << 1) | digitalRead(BL_ENC_B);
-  blCount = encoder_table[bl_enc_val & 0b1111];
+  blCount += encoder_table[bl_enc_val & 0b1111];
 }
 
 void encoder_interrupt_fr() {
   static uint8_t fr_enc_val = 0;
   fr_enc_val = (fr_enc_val << 2) | (digitalRead(FR_ENC_A) << 1) | digitalRead(FR_ENC_B);
-  frCount = encoder_table[fr_enc_val & 0b1111];
+  frCount += encoder_table[fr_enc_val & 0b1111];
 }
 
 void encoder_interrupt_fl() {
   static uint8_t fl_enc_val = 0;
   fl_enc_val = (fl_enc_val << 2) | (digitalRead(FL_ENC_A) << 1) | digitalRead(FL_ENC_B);
-  flCount = encoder_table[fl_enc_val & 0b1111];
+  flCount += encoder_table[fl_enc_val & 0b1111];
 }
